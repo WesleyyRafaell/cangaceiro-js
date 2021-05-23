a97292606223ae471986d75443d5c3f10bd6d1b0
rafael <rafaelsanto10s@gmail.com>
2021-05-22 21:25:59 -0300

    garantindo imutabilidade na classe

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,7 +1,25 @@
 class Negociacao {
   constructor( data, quantidade, valor) {
-    this.data = data;
-    this.quantidade = quantidade;
-    this.valor = valor;
+    this._data = new Date(data.getTime());
+    this._quantidade = quantidade;
+    this._valor = valor;
+    Object.freeze(this);
+  }
+
+  get volume() {
+    return this._quantidade * this._valor;
+  }
+
+  //passamos uma nova data com base na data que já foi criada no constructor (programação defensiva)
+  get data() {
+    return new Date(this._data.getTime());
+  }
+
+  get quantidade() {
+    return this._quantidade;
+  }
+
+  get valor() {
+    return this._valor;
   }
 }
