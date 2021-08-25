14dd425fb20babf3ce7b48446ab68c5c86a6995a
wesley <rafaelsanto10s@gmail.com>
2021-08-25 18:21:56 -0300

    create view for display table

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,34 +1,36 @@
 class NegociacaoController {
-  
-  constructor() {
-    let $ = document.querySelector.bind(document);
-    
-    this._inputData = $('#data');
-    this._inputQuantidade = $('#quantidade');
-    this._inputValor = $('#valor');
-		this._negociacoes = new Negociacoes();
-  }
+	constructor() {
+		let $ = document.querySelector.bind(document);
 
-  adiciona(event) {
-    event.preventDefault();
- 
-    this._negociacoes.adiciona(this._criaNegociacao());
+		this._inputData = $("#data");
+		this._inputQuantidade = $("#quantidade");
+		this._inputValor = $("#valor");
+		this._negociacoes = new Negociacoes();
+		this._negociacoesView = new NegociacoesView("#negociacoes");
+
+		this._negociacoesView.update();
+	}
+
+	adiciona(event) {
+		event.preventDefault();
+
+		this._negociacoes.adiciona(this._criaNegociacao());
 
 		this._limpaFormulario();
-  }
-	
+	}
+
 	_limpaFormulario() {
-		this._inputData.value = '';
-    this._inputQuantidade.value = 1;
-    this._inputValor.value = 0.0;
+		this._inputData.value = "";
+		this._inputQuantidade.value = 1;
+		this._inputValor.value = 0.0;
 		this._inputData.focus();
 	}
 
 	_criaNegociacao() {
 		return new Negociacao(
 			DateConverter.paraData(this._inputData.value),
-      parseInt(this._inputQuantidade.value),
-      parseFloat(this._inputValor.value)
-		)
+			parseInt(this._inputQuantidade.value),
+			parseFloat(this._inputValor.value)
+		);
 	}
 }
